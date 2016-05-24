ac51b942336f1c71338406c93a12ac3fedb9f11f
shiget <shige@sigt.jp>
2016-05-25 00:41:17 +0900

    Update third_party.php

--- includes/third_party.php
+++ includes/third_party.php
@@ -65,7 +65,7 @@ class cfs_third_party
         
         if ( is_array( $field_data ) ) {
             foreach ( $field_data as $key => $value ) {
-                delete_post_meta( $new_post_id, $key, $value );
+                delete_post_meta( $new_post_id, $key );
             }
         }
         
